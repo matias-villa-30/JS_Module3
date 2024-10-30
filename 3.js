c6e86d897d3b10344ed214dd98499b6c406f9340
villa <matias.villalemes@metropolia.fi>
2024-10-30 10:40:32 +0200

    03 valmis

---
+++ 3.js
@@ -0,0 +1,11 @@
+'use strict';
+const names = ['John', 'Paul', 'Jones'];
+let target = document.getElementById("target");
+
+
+for (let i =0;i<names.length;i++){
+  let lista = document.createElement("li");
+  lista.innerHTML = names[i];
+  target.append(lista);
+
+}
