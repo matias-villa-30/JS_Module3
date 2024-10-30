a23c687c08a76a2265e589b9c6453caa2daa5353
villa <matias.villalemes@metropolia.fi>
2024-10-30 10:28:27 +0200

    02 valmis

---
+++ 2.js
@@ -0,0 +1,15 @@
+
+let target = document.getElementById("target");
+
+
+let lista1 = document.createElement("li");
+lista1.textContent = "First Element";
+target.appendChild(lista1);
+
+let lista2 = document.createElement("li");
+lista2.textContent = "Second Element";
+target.appendChild(lista2);
+
+let lista3 = document.createElement("li");
+lista3.textContent = "Third Element";
+target.appendChild(lista3);
