328f167c513522e1339628ee0905400a7d8fb2c4
villa <matias.villalemes@metropolia.fi>
2024-10-31 17:13:23 +0200

    10 valmis

--- 10.js
+++ 10.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById('source');
 
     form.addEventListener('submit', function(evt) {
-        evt.preventDefault(); // Prevent the form from submitting
+        evt.preventDefault();
 
         let first_name = document.getElementsByName("firstname")[0];
         let last_name = document.getElementsByName("lastname")[0];
